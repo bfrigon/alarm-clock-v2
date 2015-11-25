813ccaad205a3ebbd9968ed468f87455f70e72fa
Benoit Frigon <bfrigon@gmail.com>
2015-11-25 05:53:19 -0500

    Updated mechanical drawings

--- drawings/design/README.md
+++ drawings/design/README.md
@@ -9,6 +9,19 @@ CLKV2-DWG-03  | CLKV2-PC-02       | RGB dot - PCB
 CLKV2-DWG-04  | CLKV2-ML-02       | RGB dot - Diffuser shell
 CLKV2-DWG-05  | CLKV2-PC-03       | RGB Digits display Base - PCB
 CLKV2-DWG-06  | CLKV2-PC-04       | RGB Dots Base - PCB
+CLKV2-DWG-07  | CLKV2-PC-05       | Front panel PCB
+CLKV2-DWG-08  | CLKV2-PC-06       | Touch keypad PCB
+CLKV2-DWG-09  | CLKV2-ML-03       | Touch keypad enclosure
+CLKV2-DWG-10  | CLKV2-ML-04       | Top panel vertical support
+CLKV2-DWG-11  | CLKV2-ML-05       | Front panel bracket
+CLKV2-DWG-12  | CLKV2-PC-07       | Motherboard PCB
+CLKV2-DWG-13  | CLKV2-ML-06       | Enclosure
+CLKV2-DWG-14  | CLKV2-ML-07       | Bottom cover
+CLKV2-DWG-15  | CLKV2-OVL-01      | Touch keypad overlay
 CLKV2-DWG-20  | CLKV2-AS-01       | RGB seven-segments digit assembly
 CLKV2-DWG-21  | CLKV2-AS-02       | RGB dots assembly
+CLKV2-DWG-22  | CLKV2-AS-03       | Top panel assembly
+CLKV2-DWG-23  | CLKV2-AS-04       | Motherboard assembly
+
+
 CLKV2-DWG-30  | --                | Final assembly
--- drawings/design/merge.sh
+++ drawings/design/merge.sh
@@ -33,7 +33,7 @@ combine_drawing_files () {
     for drawing_id in $(echo $2 | tr "," "\n")
     do
         #### Dump metainfo from pdf ####
-        meta=$(pdftk CLKV2-DWG-$drawing_id.pdf dump_data)
+        meta=$(pdftk clkv2-dwg-$drawing_id.pdf dump_data)
         
         #### Extract title and number of pages info ####
         title=$(echo "$meta" | sed -n '/InfoKey/ {  N ; s/InfoKey: Title\nInfoValue: \(.*\)/\1/p }')
@@ -45,7 +45,7 @@ combine_drawing_files () {
         echo "BookmarkLevel: 1" >> $TMP_METADATA_FILE
         echo "BookmarkPageNumber: $current_page" >> $TMP_METADATA_FILE
         
-        input_files="$input_files CLKV2-DWG-$drawing_id.pdf"
+        input_files="$input_files clkv2-dwg-$drawing_id.pdf"
 
         current_page=$(($current_page+$num_pages))
     done
@@ -60,21 +60,26 @@ combine_drawing_files () {
 ####################################################
 echo "Changing title metadata in pdf files..."
 
-change_drawing_title "CLKV2-DWG-01" "RGB seven-segments digit - PCB" 
-change_drawing_title "CLKV2-DWG-02" "RGB seven-segments digit - Diffuser shell" 
-change_drawing_title "CLKV2-DWG-03" "RGB dot - PCB" 
-change_drawing_title "CLKV2-DWG-04" "RGB dot - Diffuser shell" 
-change_drawing_title "CLKV2-DWG-05" "RGB Digits display Base - PCB"
-change_drawing_title "CLKV2-DWG-06" "RGB Dots Base - PCB" 
-change_drawing_title "CLKV2-DWG-20" "RGB seven-segments digit assembly" 
-change_drawing_title "CLKV2-DWG-21" "RGB dots assembly" 
-
+change_drawing_title "clkv2-dwg-01" "RGB seven-segments digit - PCB" 
+change_drawing_title "clkv2-dwg-02" "RGB seven-segments digit - Diffuser shell" 
+change_drawing_title "clkv2-dwg-03" "RGB dot - PCB" 
+change_drawing_title "clkv2-dwg-04" "RGB dot - Diffuser shell" 
+change_drawing_title "clkv2-dwg-05" "RGB Digits display Base - PCB"
+change_drawing_title "clkv2-dwg-06" "RGB Dots Base - PCB" 
+change_drawing_title "clkv2-dwg-07" "Front panel PCB"
+change_drawing_title "clkv2-dwg-08" "Touch keypad PCB"
+change_drawing_title "clkv2-dwg-09" "Touch keypad enclosure"
+change_drawing_title "clkv2-dwg-10" "Top panel vertical support"
+change_drawing_title "clkv2-dwg-11" "Front panel bracket"
+change_drawing_title "clkv2-dwg-20" "RGB seven-segments digit assembly" 
+change_drawing_title "clkv2-dwg-21" "RGB dots assembly" 
+change_drawing_title "clkv2-dwg-22" "Top panel assembly" 
 
 ####################################################
 echo "Combining pdf files for model V2..."
 
-combine_drawing_files "CLKV2-DWG" "01,02,20,03,04,21,05,06"
-change_drawing_title "CLKV2-DWG" "Alarm clock V2"
+combine_drawing_files "clkv2-dwg" "01,02,20,03,04,21,08,09,10,22,07,11,05,06"
+change_drawing_title "clkv2-dwg" "Alarm clock V2"
 
 
 echo        
